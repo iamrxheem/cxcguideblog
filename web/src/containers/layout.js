fe42c6bc31b747b9516ac91c2a7ecb95a4898a5f
iamrxheem <iamrxheem@protonmail.com>
2020-05-11 15:23:07 -0500

    Updates
    Updates

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -1,22 +1,32 @@
-import {graphql, StaticQuery} from 'gatsby'
-import React, {useState} from 'react'
-import Layout from '../components/layout'
+import { graphql, StaticQuery } from "gatsby";
+import React, { useState } from "react";
+import ReactGA from "react-ga";
+import Layout from "../components/layout";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "shards-ui/dist/css/shards.css";
+
+ReactGA.initialize("UA-165932729-1");
+
+if (process.browser) {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const query = graphql`
   query SiteTitleQuery {
-    site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
       title
     }
   }
-`
+`;
 
-function LayoutContainer (props) {
-  const [showNav, setShowNav] = useState(false)
-  function handleShowNav () {
-    setShowNav(true)
+function LayoutContainer(props) {
+  const [showNav, setShowNav] = useState(false);
+  function handleShowNav() {
+    setShowNav(true);
   }
-  function handleHideNav () {
-    setShowNav(false)
+  function handleHideNav() {
+    setShowNav(false);
   }
   return (
     <StaticQuery
@@ -25,7 +35,7 @@ function LayoutContainer (props) {
         if (!data.site) {
           throw new Error(
             'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
-          )
+          );
         }
         return (
           <Layout
@@ -35,10 +45,10 @@ function LayoutContainer (props) {
             onHideNav={handleHideNav}
             onShowNav={handleShowNav}
           />
-        )
+        );
       }}
     />
-  )
+  );
 }
 
-export default LayoutContainer
+export default LayoutContainer;
